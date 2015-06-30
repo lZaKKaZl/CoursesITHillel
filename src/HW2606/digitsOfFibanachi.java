0a553c4129eba6f5ca51914d9e4fc9d499d41cfa
lZaKKaZl <tereshko.vova@live.ru>
2015-06-30 13:55:51 +0300

    Final

---
+++ src/HW2606/digitsOfFibanachi.java
@@ -0,0 +1,20 @@
+package HW2606;
+
+/**
+ * Created by teres_000 on 30.06.2015.
+ */
+public class digitsOfFibanachi {
+    public static void main(String[] args) {
+        int n = 20;
+        int a = 1, b = 1;
+        System.out.print(a + " " + b);
+        int fib = 2, i = 2;
+        while (i < n) {
+            fib = a + b;
+            a = b;
+            b = fib;
+            System.out.print(" " + fib);
+            i++;
+        }
+    }
+}
