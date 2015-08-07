e7e4005b67d3ac440388e71d26037e7dcbe1278a
lZaKKaZl <tereshko.vova@live.ru>
2015-08-07 22:00:36 +0300

    JFrame

---
+++ src/Test1.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+/**
+ * Created by NewClass2 on 07.08.2015.
+ */
+public class Test1 extends JFrame {
+    private JPanel Perey4et;
+    private JLabel Login;
+    private JLabel Password;
+    private JPasswordField passwordField1;
+    private JTextField TextField;
+
+    public static void main(String[] args) {
+        Test1 frame = new Test1();
+
+    }
+    private void createUIComponents() {
+
+    }
+}
