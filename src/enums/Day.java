c8a1b195822effb92f39f8baf419023a491e577f
lZaKKaZl <tereshko.vova@live.ru>
2015-08-21 21:15:08 +0300

    Enum

---
+++ src/enums/Day.java
@@ -0,0 +1,14 @@
+package enums;
+
+/**
+ * Created by NewClass2 on 21.08.2015.
+ */
+public enum Day {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
